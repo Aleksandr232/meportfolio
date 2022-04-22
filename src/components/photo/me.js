e408a337b2f9a0346b3b00f1a5a581c493ad25d9
Aleksandr232 <aleksrowing232@gmail.com>
2022-04-23 00:22:03 +0300

    me

--- src/components/photo/me.js
+++ src/components/photo/me.js
@@ -1,11 +1,11 @@
 export const me = [
     {
-      src: 'https://meportfolio.vercel.app/static/media/row.c901fd202e9e7bb08d74.jpeg',
+      src: 'https://sun9-16.userapi.com/s/v1/ig2/Fc3NnSEDRS-YxdK94C4VeK4O9xeireOiObnKBLATfNQLGv5FRIkIOXc_2EDTBHZyS3f3ldJef4tCPqVyTThdAlOS.jpg?size=2560x1707&quality=96&type=album',
       width: 4,
       height: 3
     },
     {
-      src: "https://meportfolio.vercel.app/static/media/rowing.3f4610345f198705e393.jpeg",
+      src: "https://sun9-2.userapi.com/s/v1/ig2/bKOrKtiDMMSHkPXVq5mPXac0LlnqfgEIDTH_yctqosLQvv4I-Ty7BxVZKJ-RB0Hf_l0W9fY9m-z6v8PmzZ_g60vb.jpg?size=1080x810&quality=96&type=album",
       width: 1,
       height: 1
     },
@@ -15,7 +15,7 @@ export const me = [
       height: 5
     },
     {
-      src: "https://meportfolio.vercel.app/static/media/main_photo.e0291e7af039d255794e.jpg",
+      src: "https://sun9-33.userapi.com/s/v1/ig2/L0P67m5K23v-KQfEFfFzrhBsgJYm9ekWyeLD71-9N8yBK8OdLxkzr3yha31Yn3h8CzXVFV-ExYuOYtbmpDrbCHHx.jpg?size=2560x1707&quality=96&type=album",
       width: 3,
       height: 3
     },
