edcb3153d05d8094307a521591fb375dc8024feb
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-06 22:30:34 +0300

    pdf

--- src/components/me/pdf_resume/pdf_resume.js
+++ src/components/me/pdf_resume/pdf_resume.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
-/* import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'; */
+import file from './resume.pdf'
+
 
 function PdfResume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+   const [numPages, setNumPages] = useState(null); 
+    const [pageNumber, setPageNumber] = useState(1);  
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
-  }
+    setPageNumber(1)
+  } 
+  
 
+ 
   return (
     <div>
-      <Document file="resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+      <Document 
+      file={file} 
+      onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={1} />
       </Document>
       <p>
-        Page {pageNumber} of {numPages}
+      Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
       </p>
     </div>
   );
