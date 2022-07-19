5a8d54e8f742bdf0c58c01b08e6df4e87574de0e
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-19 20:29:08 +0300

    books

--- src/components/me/androidapp/androidapp.js
+++ src/components/me/androidapp/androidapp.js
@@ -6,12 +6,16 @@ import androidw from './androidw.jpg'
 import appandr from './appandr.jpg'
 import animalicon from './animalicon.png'
 import calc from './calc.png'
+import books from './books.jpg'
 
-
+const alrts=()=>{
+    return  alert('В процессе доработки')  
+}
 
 export default class Meportfolio extends Component{
-   
+    
     render(){
+        
         return(
             <section className="meportfolio" id="meportfolio">
         <h2 className="title title_fz16 resume__title circle">Портфолио</h2>
@@ -34,6 +38,10 @@ export default class Meportfolio extends Component{
             <div className="workelem"><a href="https://github.com/Aleksandr232/clacapp"><button className='wor_1'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/> </button></a>
                     <a  href='https://disk.yandex.ru/d/QnpKyXw5b922tQ'><button  className='wor'><img src="https://img.icons8.com/ios-glyphs/452/download--v2.gif" style={{width:'30px' }}/></button></a>
                         <img src={calc} width="248px" height="248px"  alt=""/>
+                        </div>
+            <div className="workelem"><a href="https://github.com/Aleksandr232/auth"><button className='wor_1'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/> </button></a>
+                        <button onClick={alrts}  className='wor'><img src="https://img.icons8.com/ios-glyphs/452/download--v2.gif" style={{width:'30px' }}/></button>
+                        <img src={books} width="248px" height="248px"  alt=""/>
                         </div>
             </div>
         </div>
